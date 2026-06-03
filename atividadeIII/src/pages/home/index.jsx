05332bcec33a84698a0b49feec534ac0bd1a1440
Victorxs05 <160060912+Victorxs05@users.noreply.github.com>
2026-06-02 21:30:05 -0300

    Add files via upload

---
+++ atividadeIII/src/pages/home/index.jsx
@@ -0,0 +1,35 @@
+import './style.css'
+
+export default function Home() {
+    return (
+        <main className="home">
+            <section id="mae">
+                <h2>Mãe!</h2>
+                <p>Obrigado por tudo, eu te amo!</p>
+            </section>
+            
+            <search id="pai">
+                <h2>Pai!</h2>
+                <p>Obrigado por todos ensinamentos, você é minha maior inspiração, eu te amo!</p>
+            </search>
+            
+            <search id="Fotos">
+                <h2>Fotos Familia</h2>
+                <div className="fotos-familia">
+                    <div className="foto">Foto 1</div>
+                    <div className="foto">Foto 2</div>
+                    <div className="foto">Foto 3</div>
+                </div>
+            </search>
+
+            <search id="Videos">
+                <h2>Videos Familia</h2>
+                <div className="videos-familia">
+                <video controls width="400">
+                <source src="/assets/video.mp4" type="video/mp4" />
+                </video>
+                </div>
+            </search>
+        </main>
+    );
+}
