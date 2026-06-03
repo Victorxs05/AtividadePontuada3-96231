dc438990ebaa2a35a8edf45b147e0dfc46d11a86
Victorxs05 <160060912+Victorxs05@users.noreply.github.com>
2026-06-03 14:11:11 -0300

    Update index.jsx

--- atividadeIII/src/pages/home/index.jsx
+++ atividadeIII/src/pages/home/index.jsx
@@ -8,28 +8,28 @@ export default function Home() {
                 <p>Obrigado por tudo, eu te amo!</p>
             </section>
             
-            <search id="pai">
+            <section id="pai">
                 <h2>Pai!</h2>
                 <p>Obrigado por todos ensinamentos, você é minha maior inspiração, eu te amo!</p>
-            </search>
+            </section>
             
-            <search id="Fotos">
+            <section id="Fotos">
                 <h2>Fotos Familia</h2>
                 <div className="fotos-familia">
                     <div className="foto">Foto 1</div>
                     <div className="foto">Foto 2</div>
                     <div className="foto">Foto 3</div>
                 </div>
-            </search>
+            </section>
 
-            <search id="Videos">
+            <section id="Videos">
                 <h2>Videos Familia</h2>
                 <div className="videos-familia">
                 <video controls width="400">
                 <source src="/assets/video.mp4" type="video/mp4" />
                 </video>
                 </div>
-            </search>
+            </section>
         </main>
     );
 }
